5d89d2cf23902fa75789c1ba18ba5cecd1f518f3
Pim <30662877+PimMiii@users.noreply.github.com>
2023-05-24 15:49:10 +0200

    Created WaterParam class. Added temporary class debugging in game.ts

---
+++ src/WaterParam.ts
@@ -0,0 +1,56 @@
+import * as PIXI from "pixi.js";
+
+/**
+ * Class to model a water quality parameter.
+ *
+ * @param name - Display name of the parameter.
+ * @param key - given key of the parameter in the NN-model.
+ * @param value - value of the parameter'.
+ * @param increment - the increment with which the value changes.
+ *
+ */
+export class WaterParam {
+  public name: string; // displayname of parameter
+  private key: string; // name of parameter in model
+  private value: number; // initial value of parameter
+  private increment: number; // value with which to change the param's value per step (*1)
+
+  constructor(name: string, key: string, value: number, increment: number) {
+    this.name = name;
+
+    this.key = key;
+
+    this.value = value;
+
+    this.increment = increment;
+
+    // sanity check DEBUG ONLY
+    console.log(
+      `WaterParam created: ${this.name} (${this.key}), value: ${this.value} (${this.increment})`
+    );
+  }
+
+  /**
+   * function to return a NN-friendly object with keyname and value
+   *
+   * @returns object with a 'key' and a 'value' field.
+   *
+   */
+  getKeyValue() {
+    return { key: this.key, value: this.value };
+  }
+
+  update(step: number) {
+    if (step != null && step >= -5 && step <= 5) {
+      this.value += this.increment * step;
+      if (this.value < 0) {
+        this.value = 0;
+      }
+      console.log(this.value);
+    } else {
+      console.log(
+        `Could not update WaterParam.value. Invalid step value. (range: -5 - 5, given: ${step})`
+      );
+    }
+  }
+}
--- src/game.ts
+++ src/game.ts
@@ -1,33 +1,41 @@
 // Import PIXI
-import * as PIXI from 'pixi.js';
-import { AssetLoader } from './AssetLoader';
-import { Player } from './Player';
+import * as PIXI from "pixi.js";
+import { AssetLoader } from "./AssetLoader";
+import { Player } from "./Player";
+import { WaterParam } from "./WaterParam";
 
 export class Game {
-    public pixi: PIXI.Application
-    private loader: AssetLoader
-    public player: Player
-    private gameTexture: PIXI.Texture
-    private officeAssets: PIXI.Texture
+  public pixi: PIXI.Application;
+  private loader: AssetLoader;
+  public player: Player;
+  private gameTexture: PIXI.Texture;
+  private officeAssets: PIXI.Texture;
 
-    constructor() {
-        this.pixi = new PIXI.Application()
-        document.body.appendChild(this.pixi.view)
-        // Load images
-        this.loader = new AssetLoader(this)
-    }
+  private waterParamA: WaterParam;
 
-    loadCompleted() {
-        console.log("Load completed")
-        console.log(this.loader.textures)
+  constructor() {
+    this.pixi = new PIXI.Application();
+    document.body.appendChild(this.pixi.view);
+    // Load images
+    this.loader = new AssetLoader(this);
+  }
 
-        this.gameTexture = this.loader.textures.Player['flowerTop']
-        this.officeAssets = this.loader.textures.Office
-        console.log(this.officeAssets)
+  loadCompleted() {
+    console.log("Load completed");
+    console.log(this.loader.textures);
 
-        this.player = new Player(this.gameTexture)
-        this.pixi.stage.addChild(this.player)
-    }
+    this.gameTexture = this.loader.textures.Player["flowerTop"];
+    this.officeAssets = this.loader.textures.Office;
+    console.log(this.officeAssets);
+
+    this.player = new Player(this.gameTexture);
+    this.pixi.stage.addChild(this.player);
+
+    // PARAM TESTING DEBUG ONLY
+    this.waterParamA = new WaterParam("Parameter A", "parameter_a", 10, 1);
+    this.waterParamA.update(-1);
+    console.log(this.waterParamA.getKeyValue());
+  }
 }
 
 new Game();
