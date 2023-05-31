30603ec57bd6e1a1b70cdaf5e38bdf799e33c5fd
Quinten vD <89836942+QuintenvDriel@users.noreply.github.com>
2023-05-31 12:39:54 +0200

    Als waterschap lid wil ik dat spelers informatieve informatie krijgen zodat zij hun steentje bij kunnen dragen. Fixes #1

--- src/game.ts
+++ src/game.ts
@@ -2,32 +2,55 @@
 import * as PIXI from 'pixi.js';
 import { AssetLoader } from './AssetLoader';
 import { Player } from './Player';
+import { PopUp } from './tip-popUp';
 
 export class Game {
-    public pixi: PIXI.Application
-    private loader: AssetLoader
-    public player: Player
-    private gameTexture: PIXI.Texture
-    private officeAssets: PIXI.Texture
+  public pixi: PIXI.Application
+  private loader: AssetLoader
+  public player: Player
+  private gameTexture: PIXI.Texture
+  private officeAssets: PIXI.Texture
+  private popUp: PopUp
 
-    constructor() {
-        this.pixi = new PIXI.Application()
-        document.body.appendChild(this.pixi.view)
-        // Load images
-        this.loader = new AssetLoader(this)
-    }
+  constructor() {
+    this.pixi = new PIXI.Application()
+    document.body.appendChild(this.pixi.view)
+    // Load images
+    this.loader = new AssetLoader(this)
+    this.popUp = new PopUp(this.pixi);
+    
+  }
 
-    loadCompleted() {
-        console.log("Load completed")
-        console.log(this.loader.textures)
+  loadCompleted() {
+    console.log("Load completed")
+    console.log(this.loader.textures)
 
-        this.gameTexture = this.loader.textures.Player['flowerTop']
-        this.officeAssets = this.loader.textures.Office
-        console.log(this.officeAssets)
+    // this.gameTexture = this.loader.textures.Player['flowerTop']
+    this.officeAssets = this.loader.textures.Office
+    console.log(this.officeAssets)
 
-        this.player = new Player(this.gameTexture)
-        this.pixi.stage.addChild(this.player)
-    }
+    this.player = new Player(this.gameTexture)
+    this.pixi.stage.addChild(this.player)
+    this.pixi.stage.addChild(this.popUp.container)
+
+    const openButton = new PIXI.Graphics();
+    openButton.beginFill(0xffffff);
+    openButton.drawRect(-70, -70, 100, 50);
+    openButton.endFill();
+    openButton.interactive = true;
+    openButton.buttonMode = true;
+    openButton.x = 100;
+    openButton.y = 100;
+    this.pixi.stage.addChild(openButton);
+
+    openButton.on("click", () => {
+      this.popUp.open();
+    });
+
+    this.popUp.addTip("De waterkwaliteit is omhoog gegaan");
+    this.popUp.addTip("De dijken zijn nu zwakker");
+    this.popUp.addTip("Placeholder tekst");
+  }
 }
 
 new Game();
---
+++ src/tip-popUp.ts
@@ -0,0 +1,63 @@
+import * as PIXI from "pixi.js"
+
+export class PopUp {
+
+        constructor(pixi) {
+          this.container = new PIXI.Container();
+          this.container.visible = false;
+      
+          // Achtergrondoverlay
+          const overlay = new PIXI.Graphics();
+          overlay.beginFill(0x000000, 0.5);
+          overlay.drawRect(0, 0, window.innerWidth, window.innerHeight);
+          overlay.endFill();
+          this.container.addChild(overlay);
+      
+          // Pop-up content
+          const content = new PIXI.Container();
+          const canvasWidth = pixi.renderer.width;
+          const canvasHeight = pixi.renderer.height;
+          content.position.set(
+            (canvasWidth - content.width) / 2,
+            (canvasHeight - content.height) / 2
+          );
+          this.container.addChild(content);
+      
+          // Sluitknop
+          const closeButton = new PIXI.Text("X", { fill: "#ffffff" });
+          closeButton.anchor.set(1, 0);
+          closeButton.position.set(150, -150);
+          closeButton.interactive = true;
+          closeButton.buttonMode = true;
+          closeButton.on("click", this.close.bind(this));
+          content.addChild(closeButton);
+      
+          // Tipslijst
+          this.tipsList = new PIXI.Container();
+          this.tipsList.position.set(-150, -100);
+          content.addChild(this.tipsList);
+        }
+
+      
+      
+      
+        open() {
+          this.container.visible = true;
+        }
+      
+        close() {
+          this.container.visible = false;
+        }
+      
+        addTip(tipText) {
+          const tipItem = new TipItem(tipText);
+          tipItem.container.position.y = this.tipsList.children.length * 40;
+          this.tipsList.addChild(tipItem.container);
+        }
+      }
+
+    class TipItem {
+      constructor(text) {
+        this.container = new PIXI.Text(text, { fill: "#ffffff" });
+      }
+    }
