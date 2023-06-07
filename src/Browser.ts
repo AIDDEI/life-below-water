bb3e82257c0b5bbd6f192628460fc46c0a5e854a
Isis <1036029@hr.nl>
2023-06-07 22:50:19 +0200

    Nicer window image + search bar text

--- src/Browser.ts
+++ src/Browser.ts
@@ -1,166 +1,167 @@
-
-import { Container, Graphics, Rectangle, Sprite, Text, Texture } from 'pixi.js'
-import { MailScreen } from './MailScreen'
+import { Container, Graphics, Rectangle, Sprite, Text, Texture } from "pixi.js";
+import { MailScreen } from "./MailScreen";
 
 /**
  * Class for the browser container and tabs
  *
  * @param texture - texture for the background of the browser
- * 
+ *
  * @example
  *  const browser = new Browser(PIXI.Texture.from('browser.png'))
  *
  */
 export class Browser extends Container {
-    private tabContainer: Container
-    private _openTab: number
-    private _lastOpenTab: number
-    private bgContainer: any
-    private _tabs: any
+	private tabContainer: Container;
+	private _openTab: number;
+	private _lastOpenTab: number;
+	private bgContainer: any;
+	private _tabs: any;
+	private _searchBarText: Text;
 
-    constructor(texture: Texture) {
-        super()
-        this.x = 0
-        this.y = 0
-        this.tabContainer = new Container()
-        this.tabContainer.x = 0
-        this.tabContainer.y = 10
-        // mail open by default
-        this._openTab = -1
-        this._lastOpenTab = -1
-        this._tabs = []
+	constructor(texture: Texture) {
+		super();
+		this.x = 0;
+		this.y = 0;
+		this.tabContainer = new Container();
+		this.tabContainer.x = 0;
+		this.tabContainer.y = 10;
+		// mail open by default
+		this._openTab = -1;
+		this._lastOpenTab = -1;
+		this._searchBarText = new Text("", {
+			fontFamily: "Arial",
+			fontSize: 18,
+			fill: 0x000000,
+			align: "center",
+		});
+		this._searchBarText.x = 185;
+		this._searchBarText.y = 65;
+		this._tabs = [];
+		this._setUpBackground(texture);
+		this.addChild(this._searchBarText, this.tabContainer);
+	}
 
-        this._setUpBackground(texture)
-        this.addChild(this.tabContainer)
+	private _setUpBackground(texture: Texture) {
+		const bgTexture = new Sprite(texture);
+		this.bgContainer = new Container();
 
-    }
+		bgTexture.x = 0;
+		bgTexture.y = 0;
+		bgTexture.width = 800;
+		bgTexture.height = 600;
 
+		this.bgContainer.addChild(bgTexture);
+		this.addChild(this.bgContainer);
+	}
 
-    private _setUpBackground(texture: Texture) {
-        const bgTexture = new Sprite(texture)
-        this.bgContainer = new Container()
+	/**
+	 * function to add a browser tab
+	 * @param tabName (string) - name of the tab
+	 * @param screen (opional | component) - screen to be rendered when the tab is clicked. MUST have public open and public close methods
+	 *
+	 *  Renders the tab and adds the screen to the browser
+	 *
+	 * @example
+	 *  this.browser.addTab('Mail', this.mail)
+	 *  this.browser.addTab('Placeholder', undefined)
+	 */
+	// TODO: add types of the screens that are expected to be added
+	public addTab(tabName: string, screen: MailScreen | any) {
+		if ((screen && screen.open == undefined) || (screen && screen.close == undefined)) return console.error("Invalid screen! Must have public open and public close methods");
+		if (this._tabs.length >= 5) return console.error("Too many tabs! 5 is the maximum");
 
-        bgTexture.x = 0
-        bgTexture.y = 0
-        bgTexture.width = 800
-        bgTexture.height = 600
+		this._tabs.push({ tabName: tabName, screen: screen });
 
-        this.bgContainer.addChild(bgTexture)
-        this.addChild(this.bgContainer)
-    }
+		if (screen) this.addChild(screen);
 
-    public set openTab(tab: number) {
-        if (this._tabs[tab]?.screen === undefined) {
-            console.error('Invalid tab! Either has no screen or does not exist.')
-        } else {
-            this._openTab = tab
-            this._renderTabs()
-            this._renderWindow()
-        }
-    }
+		this._renderTabs();
+	}
 
-    /**
-* function to add a browser tab
-* @param tabName (string) - name of the tab
-* @param screen (opional | component) - screen to be rendered when the tab is clicked. MUST have public open and public close methods
-* 
-*  Renders the tab and adds the screen to the browser
-*
-* @example
-*  this.browser.addTab('Mail', this.mail)
-*  this.browser.addTab('Placeholder', undefined)
-*/
-    // TODO: add types of the screens that are expected to be added
-    public addTab(tabName: string, screen: MailScreen | any) {
-        if (screen && screen.open == undefined || screen && screen.close == undefined) return console.error('Invalid screen! Must have public open and public close methods')
-        if (this._tabs.length >= 5) return console.error('Too many tabs! 5 is the maximum')
+	private _renderTabs() {
+		this._tabs.forEach((tab: any, i: number) => {
+			const tabContainer = new Container();
 
-        this._tabs.push({ tabName: tabName, screen: screen })
+			tabContainer.x = 15 + 75 * i;
+			tabContainer.hitArea = new Rectangle(tabContainer.x, this.tabContainer.y, 140, 30);
+			tabContainer.eventMode = "static";
 
-        if (screen) this.addChild(screen)
+			const tabBackground = new Graphics();
+			// if the tab is open, fill it with white, otherwise fill it with grey
+			const fill = i === this._openTab ? 0xffffff : 0x929292;
+			tabBackground.beginFill(fill);
+			tabBackground.drawRoundedRect(tabContainer.x, 10, 140, 30, 3);
+			tabBackground.endFill();
+			tabContainer.addChild(tabBackground);
 
-        this._renderTabs()
-    }
+			const tabText = new Text(tab.tabName, {
+				fontFamily: "Arial",
+				fontSize: 18,
+				fill: 0x000000,
+				align: "center",
+			});
 
+			tabText.x = tabContainer.x + 10;
+			tabText.y = 10 + 30 / 2 - tabText.height / 2;
 
-    private _renderTabs() {
+			// x to the right of the tab
+			const x = new Text("x", {
+				fontFamily: "Arial",
+				fontSize: 15,
+				fill: 0x000000,
+				align: "center",
+			});
 
-        this._tabs.forEach((tab, i: number) => {
-            const tabContainer = new Container()
+			x.x = tabContainer.x + tabContainer.width - 20;
+			x.y = 10 + 30 / 2 - x.height / 2;
 
-            tabContainer.x = 15 + 75 * i
-            tabContainer.hitArea = new Rectangle(tabContainer.x, this.tabContainer.y, 140, 30)
-            tabContainer.eventMode = 'static'
+			tabBackground.addChild(tabText, x);
+			this.tabContainer.addChild(tabContainer);
 
-            const tabBackground = new Graphics()
-            // if the tab is open, fill it with white, otherwise fill it with grey
-            const fill = (i === this._openTab) ? 0xFFFFFF : 0x929292
-            tabBackground.beginFill(fill)
-            tabBackground.drawRoundedRect(tabContainer.x, 10, 140, 30, 3)
-            tabBackground.endFill()
-            tabContainer.addChild(tabBackground)
+			// if placehlder, don't add event listeners
+			if (!tab.screen) return;
 
-            const tabText = new Text(tab.tabName, {
-                fontFamily: 'Arial',
-                fontSize: 18,
-                fill: 0x000000,
-                align: 'center'
-            })
+			tabContainer.cursor = "pointer";
 
-            tabText.x = tabContainer.x + 10
-            tabText.y = 10 + 30 / 2 - tabText.height / 2
+			// add event listeners
+			tabContainer.onclick = () => {
+				if (i === this._openTab) return;
+				this.openTab = i;
+			};
 
-            // x to the right of the tab
-            const x = new Text('x', {
-                fontFamily: 'Arial',
-                fontSize: 15,
-                fill: 0x000000,
-                align: 'center'
-            })
+			if (i !== this._openTab) {
+				tabContainer.onmouseover = () => {
+					// tint over tab
+					tabBackground.tint = 0xc9c9c9;
+				};
 
-            x.x = tabContainer.x + tabContainer.width - 20
-            x.y = 10 + 30 / 2 - x.height / 2
+				tabContainer.onmouseout = () => {
+					// remove tint over tab
+					tabBackground.tint = 0xffffff;
+				};
+			}
+		});
+	}
 
-            tabBackground.addChild(tabText, x)
-            this.tabContainer.addChild(tabContainer)
+	public set openTab(tab: number) {
+		if (this._tabs[tab]?.screen === undefined) {
+			console.error("Invalid tab! Either has no screen or does not exist.");
+		} else {
+			this._openTab = tab;
+			this._renderTabs();
+			this._renderSearchBar();
+			this._renderWindow();
+		}
+	}
+	private _renderSearchBar() {
+		this._searchBarText.text = this._tabs[this._openTab].tabName.toUpperCase();
+	}
 
-            // if placehlder, don't add event listeners
-            if (!tab.screen) return
+	private _renderWindow() {
+		if (this._lastOpenTab !== -1) {
+			this._tabs[this._lastOpenTab].screen.close();
+		}
 
-            tabContainer.cursor = 'pointer'
-
-            // add event listeners
-            tabContainer.onclick = () => {
-                if (i === this._openTab) return
-                this._changeTab(i)
-            }
-
-            if (i !== this._openTab) {
-                tabContainer.onmouseover = () => {
-                    // tint over tab
-                    tabBackground.tint = 0xC9C9C9
-                }
-
-                tabContainer.onmouseout = () => {
-                    // remove tint over tab
-                    tabBackground.tint = 0xFFFFFF
-                }
-            }
-        })
-    }
-
-    private _changeTab(tab: number) {
-        this._openTab = tab
-        this._renderTabs()
-        this._renderWindow()
-    }
-
-    private _renderWindow() {
-        if (this._lastOpenTab !== -1) {
-            this._tabs[this._lastOpenTab].screen.close()
-        }
-
-        this._tabs[this._openTab].screen.open()
-        this._lastOpenTab = this._openTab
-    }
-
+		this._tabs[this._openTab].screen.open();
+		this._lastOpenTab = this._openTab;
+	}
+}
--- src/game.ts
+++ src/game.ts
@@ -4,126 +4,108 @@ import { Player } from "./Player";
 import { MailScreen } from "./MailScreen";
 import { WaterParam } from "./WaterParam";
 import { Calendar } from "./Calendar";
-import { LobGame } from './LobGame';
+import { LobGame } from "./LobGame";
 import { Browser } from "./Browser";
 
-export type AssetType = { [key: string]: PIXI.Texture<PIXI.Resource> }
+export type AssetType = { [key: string]: PIXI.Texture<PIXI.Resource> };
 
 export class Game {
-    public pixi: PIXI.Application;
-    private loader: AssetLoader;
-    public player: Player;
-    private gameTexture: PIXI.Texture;
-    public mail: MailScreen;
-    public lobGame: LobGame | undefined;
-    private lobAssets: PIXI.Texture<PIXI.Resource>
-    private officeAssets: PIXI.Texture;
-    private mailAssets: PIXI.Texture[];
-    private dayAssets: any;
-    public calendar: Calendar;
-    //water parameters related
-    public waterParameters: WaterParam[];
-    private waterParamA: WaterParam;
-    private waterParamB: WaterParam;
-    private waterParamC: WaterParam;
-    browser: Browser;
+	public pixi: PIXI.Application;
+	private loader: AssetLoader;
+	public player: Player;
+	private gameTexture: PIXI.Texture;
+	public mail: MailScreen;
+	public lobGame: LobGame | undefined;
+	private lobAssets: PIXI.Texture<PIXI.Resource>;
+	private officeAssets: PIXI.Texture;
+	private mailAssets: PIXI.Texture[];
+	private dayAssets: any;
+	public calendar: Calendar;
+	//water parameters related
+	public waterParameters: WaterParam[];
+	private waterParamA: WaterParam;
+	private waterParamB: WaterParam;
+	private waterParamC: WaterParam;
+	browser: Browser;
 
-    constructor() {
-        PIXI.settings.ROUND_PIXELS = true;
+	constructor() {
+		PIXI.settings.ROUND_PIXELS = true;
 
-        // init game
-        this.pixi = new PIXI.Application({
-            autoDensity: true,
-            resolution: window.devicePixelRatio,
-        });
+		// init game
+		this.pixi = new PIXI.Application({
+			autoDensity: true,
+			resolution: window.devicePixelRatio,
+		});
 
-        document.body.appendChild(this.pixi.view as HTMLCanvasElement);
-        // Load images
-        this.loader = new AssetLoader(this);
+		document.body.appendChild(this.pixi.view as HTMLCanvasElement);
+		// Load images
+		this.loader = new AssetLoader(this);
 
-        this.waterParamA = new WaterParam("Parameter A", "parameter_a", -1, 1);
-        this.waterParamB = new WaterParam("Parameter B", "parameter_b", 100, 11);
-        this.waterParamC = new WaterParam(
-            "Parameter C",
-            "parameter_c",
-            624,
-            20,
-            500,
-            700
-        );
-    }
+		this.waterParamA = new WaterParam("Parameter A", "parameter_a", -1, 1);
+		this.waterParamB = new WaterParam("Parameter B", "parameter_b", 100, 11);
+		this.waterParamC = new WaterParam("Parameter C", "parameter_c", 624, 20, 500, 700);
+	}
 
-    loadCompleted() {
+	loadCompleted() {
+		console.log("Load completed");
+		console.log(this.loader.textures);
 
-        console.log("Load completed");
-        console.log(this.loader.textures);
+		this.officeAssets = this.loader.textures.Office;
+		this.mailAssets = this.loader.textures.MailScreen;
+		this.dayAssets = this.loader.textures.DayScreen;
+		this.lobAssets = this.loader.textures.Lobgame;
 
-        this.officeAssets = this.loader.textures.Office;
-        this.mailAssets = this.loader.textures.MailScreen;
-        this.dayAssets = this.loader.textures.DayScreen;
-        this.lobAssets = this.loader.textures.Lobgame
+		this.calendar = new Calendar(this.dayAssets, this);
+		// this.player = new Player(this.gameTexture)
+		// this.pixi.stage.addChild(this.player)
 
+		this.mail = new MailScreen(this.mailAssets, this);
+		this.browser = new Browser(this.loader.textures.browser);
 
-        this.calendar = new Calendar(this.dayAssets, this)
-        // this.player = new Player(this.gameTexture)
-        // this.pixi.stage.addChild(this.player)
+		this.browser.addTab("Kwaliteit", undefined);
+		this.browser.addTab("E-mail", this.mail);
+		this.browser.addTab("Kaart", undefined);
+		this.browser.addTab("Over ons", this.mail);
 
-        this.mail = new MailScreen(this.mailAssets, this);
-        this.browser = new Browser(this.loader.textures.browser)
+		this.browser.openTab = 1;
+		this.pixi.stage.addChild(this.browser);
 
-        this.browser.addTab("Kwaliteit", undefined)
-        this.browser.addTab("E-mail", this.mail)
-        this.browser.addTab("Kaart", undefined)
-        this.browser.addTab("Over ons", undefined)
+		this.mail.add(
+			"Lob lob lob",
+			"De zomer is in aantocht het beloofd een warme en droge zomer te worden. Ons doel is om onze inwoners schoon en veilig zwemwater te kunnen bieden. Zodat zij het hoofd koel kunnen houden! \n\nJouw doel voor de komende week is; de waterkwaliteit verbeteren.",
+			0,
+			true,
+			"lob"
+		);
+		this.mail.add("Mail 1", "This is the first maiwadawdawdwad wdmwaidmwa idmawid dadwad wl.", 0, false, "lob");
+		this.mail.add("Mail 3", "This is the third mail.", 0, false, "lob");
+		this.mail.add("Mail 4", "This is the third mail.", 0);
 
-        this.browser.openTab = 1
-        this.pixi.stage.addChild(this.browser)
+		// ! Keep this last
+		this.pixi.stage.addChild(this.calendar);
 
+		this.pixi.ticker.add((delta) => this.update(delta));
+	}
 
-        this.mail.add(
-            "Lob lob lob",
-            "De zomer is in aantocht het beloofd een warme en droge zomer te worden. Ons doel is om onze inwoners schoon en veilig zwemwater te kunnen bieden. Zodat zij het hoofd koel kunnen houden! \n\nJouw doel voor de komende week is; de waterkwaliteit verbeteren.",
-            0,
-            true,
-            "lob"
-        );
-        this.mail.add(
-            "Mail 1",
-            "This is the first maiwadawdawdwad wdmwaidmwa idmawid dadwad wl.",
-            0,
-            false,
-            "lob"
-        );
-        this.mail.add("Mail 3", "This is the third mail.", 0, false, "lob");
-        this.mail.add("Mail 4", "This is the third mail.", 0);
+	private update(delta: number) {
+		// this.player.update(delta)
+		// this.mail.update(delta)
 
+		if (this.lobGame?.active) this.lobGame.update(delta);
+	}
 
-        // ! Keep this last 
-        this.pixi.stage.addChild(this.calendar)
+	public startLobGame() {
+		this.mail.visible = false;
+		this.lobGame = new LobGame(this.lobAssets, this);
+		this.pixi.stage.addChild(this.lobGame);
+	}
 
-        this.pixi.ticker.add((delta) => this.update(delta))
-    }
-
-    private update(delta: number) {
-        // this.player.update(delta)
-        // this.mail.update(delta)
-
-        if (this.lobGame?.active) this.lobGame.update(delta)
-    }
-
-    public startLobGame() {
-        this.mail.visible = false;
-        this.lobGame = new LobGame(this.lobAssets, this);
-        this.pixi.stage.addChild(this.lobGame);
-
-    }
-
-    public endLobGame(score: number, reason: number): void {
-        if (this.lobGame) this.pixi.stage.removeChild(this.lobGame);
-        this.lobGame = undefined;
-        this.mail.visible = true;
-        this.mail.add('Lob lob lob', `End of lob. ${score}`, 0, true);
-    }
+	public endLobGame(score: number, reason: number): void {
+		if (this.lobGame) this.pixi.stage.removeChild(this.lobGame);
+		this.lobGame = undefined;
+		this.mail.visible = true;
+		this.mail.add("Lob lob lob", `End of lob. ${score}`, 0, true);
+	}
 }
 
 new Game();
