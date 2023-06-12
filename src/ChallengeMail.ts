6201a8f7735459cec4f968b80480aff1543d8ca4
Isis <1036029@hr.nl>
2023-06-12 14:25:06 +0200

    WIP, add scrollbar

--- src/ChallengeMail.ts
+++ src/ChallengeMail.ts
@@ -1,44 +1,44 @@
-import * as PIXI from 'pixi.js';
-import { ActiveMail } from './ActiveMail';
-import { Button } from './Button';
-import { Game } from './game';
+import * as PIXI from "pixi.js";
+import { ActiveMail } from "./ActiveMail";
+import { Button } from "./Button";
+import { Game } from "./game";
 
 export class ChallengeMail extends ActiveMail {
+	constructor(mail: any, mailHeaderIcon: PIXI.Sprite, game: Game) {
+		super(mail, mailHeaderIcon, game);
+		this.createContent();
 
- 
-    constructor(mail: any, mailHeaderIcon: PIXI.Sprite, game: Game) {
-        super(mail, mailHeaderIcon, game);
-        this.createContent();
-    }
+		// after creating the content, check if the content is too big for the mail
+		this.checkScroll();
+	}
 
-    private createContent() {
-        const contentText = new PIXI.Text(this.mail.description, { fill: 'blue', fontSize: 15 });
-        contentText.style.wordWrap = true;
-        contentText.style.wordWrapWidth = this.width - 25;
+	private createContent() {
+		const contentText = new PIXI.Text(this.mail.description, { fill: "blue", fontSize: 15 });
+		contentText.style.wordWrap = true;
+		contentText.style.wordWrapWidth = this.width - 25;
 
-        contentText.position.set(this.x + 20, 150);
+		contentText.position.set(this.x + 20, 150);
 
-        // Add the content to the content container
-        this.addChild(contentText);
+		// Add the content to the content container
+		this.contentContainer.addChild(contentText);
 
-        let button: Button;
+		let button: Button;
 
-        switch (this.mail.identifier) {
-            case 'lob':
-                if (this.mail.played) return
-                button = new Button(50, 'Accepteer missie', undefined, undefined, () => {
-                    this.game.startLobGame();
-                    this.game.mail.mails[this.mail.index].played = true;
-                });
-                break;
-            default:
-                button = new Button(50, 'Dit hoort niet..', undefined, undefined, () => {
-                    console.log('This should not happen');
-                });
-        }
+		switch (this.mail.identifier) {
+			case "lob":
+				if (this.mail.played) return;
+				button = new Button(50, "Accepteer missie", undefined, undefined, () => {
+					this.game.startLobGame();
+					this.game.mail.mails[this.mail.index].played = true;
+				});
+				break;
+			default:
+				button = new Button(50, "Dit hoort niet..", undefined, undefined, () => {
+					console.log("This should not happen");
+				});
+		}
 
-        button.position.set(this.x + 20, this.height - button.height / 2 - 5);
-        this.addChild(button);
-    }
+		button.position.set(this.x + 20, this.contentContainer.height + button.height + 10);
+		this.contentContainer.addChild(button);
+	}
 }
-
--- src/ResultMail.ts
+++ src/ResultMail.ts
@@ -1,34 +1,35 @@
-import * as PIXI from 'pixi.js';
-import { ActiveMail } from './ActiveMail';
-import { Button } from './Button';
+import * as PIXI from "pixi.js";
+import { ActiveMail } from "./ActiveMail";
+import { Button } from "./Button";
 
 export class ResultMail extends ActiveMail {
+	constructor(mail: any, mailHeaderIcon: PIXI.Sprite, game: any) {
+		super(mail, mailHeaderIcon, game);
+		this.createContent();
 
-  constructor(mail: any, mailHeaderIcon: PIXI.Sprite, game: any) {
-    super(mail, mailHeaderIcon, game);
-    this.createContent();
+		// after creating the content, check if the content is too big for the mail
+		this.checkScroll();
+	}
 
-  }
+	private createContent() {
+		let salary = 0;
 
-  private createContent() {
-    let salary = 0;
+		salary = this.mail.reason == 0 ? 150 : 75;
 
-    salary = this.mail.reason == 0 ? 150 : 75;
+		const salaryText = new PIXI.Text(`Je ontvangt: €${salary}`, { fill: "blue", fontSize: 15 });
+		salaryText.position.set(this.x + 20, 180);
 
-    const salaryText = new PIXI.Text(`Je ontvangt: €${salary}`, { fill: 'blue', fontSize: 15 });
-    salaryText.position.set(this.x + 20, 180);
+		const reasonText = new PIXI.Text(`Bedankt voor het voltooien van deze opdracht! \n${this.mail.description}.`, { fill: "black", fontSize: 15 });
+		reasonText.position.set(this.x + 20, 210);
+		reasonText.style.wordWrap = true;
+		reasonText.style.wordWrapWidth = this.width - 25;
 
-    const reasonText = new PIXI.Text(`Bedankt voor het voltooien van deze opdracht! \n${this.mail.description}.`, { fill: 'black', fontSize: 15 });
-    reasonText.position.set(this.x + 20, 210);
-    reasonText.style.wordWrap = true;
-    reasonText.style.wordWrapWidth = this.width - 25;
+		const button = new Button(50, "Doorgaan", 0xffbd01, 0x336699, () => {
+			console.log("clicked!");
+		});
+		button.position.set(this.x + 20, this.height - button.height / 2 - 5);
 
-    const button = new Button(50, 'Doorgaan', 0xFFBD01, 0x336699, () => { console.log('clicked!') });
-    button.position.set(this.x + 20, this.height - button.height / 2 - 5);
-
-
-    // Add the content to the content container
-    this.addChild(salaryText, reasonText, button)
-
-  }
+		// Add the content to the content container
+		this.addChild(salaryText, reasonText, button);
+	}
 }
