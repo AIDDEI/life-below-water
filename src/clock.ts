543c9ed9f5a67b418e4c052fff867aafcfa45b10
Quinten vD <89836942+QuintenvDriel@users.noreply.github.com>
2023-06-20 11:13:31 +0200

    .

--- src/clock.ts
+++ src/clock.ts
@@ -50,9 +50,27 @@ export class Clock extends PIXI.Container{
     }
 
 
+    // Getallen op de wijzerplaat
+    const numberStyle = new PIXI.TextStyle({
+      fontSize: 50,
+      fill: 'black',
+    });
+
+    for (let hour = 1; hour <= 12; hour++) {
+      const angle = (hour / 12) * Math.PI * 2; // Hoek voor elk uur
+      const radius = 150;
+      const x = 200 + radius * Math.sin(angle);
+      const y = 200 - radius * Math.cos(angle);
+
+      const numberText = new PIXI.Text(hour.toString(), numberStyle);
+      numberText.anchor.set(0.5);
+      numberText.position.set(x, y);
+      this.clockGraphics.addChild(numberText); 
+    }
+
     // Lange wijzer (minuten wijzer)
     const longHandRotation = (this.halfHourOffset / 180) * Math.PI;
-    this.clockGraphics.lineStyle(12, 0xff0000);
+    this.clockGraphics.lineStyle(9, 0xff0000);
     this.clockGraphics.moveTo(200, 200);
     this.clockGraphics.lineTo(200 + 150 * Math.sin(longHandRotation), 200 - 150 * Math.cos(longHandRotation));
 
