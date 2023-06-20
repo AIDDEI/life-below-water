e324ca0afde902ca9d1d6c19e108ef6d8cd10f1c
Quinten vD <89836942+QuintenvDriel@users.noreply.github.com>
2023-06-20 11:56:59 +0200

    Als waterschap lid wil ik dat spelers informatieve informatie krijgen zodat zij hun steentje bij kunnen dragen. Fixes #1

--- src/clock.ts
+++ src/clock.ts
@@ -18,8 +18,8 @@ export class Clock extends PIXI.Container{
     this.drawClock(); // Tekenen van de klok bij initialisatie
     this.clockContainer.addChild(this.clockGraphics);
     this.addChild(this.clockContainer);
-    this.clockContainer.position.set(20, 10); // Positie van de klok
-    this.clockContainer.scale.set(0.25); // Formaat van de klok
+    this.clockContainer.position.set(700, 40); // Positie van de klok
+    this.clockContainer.scale.set(0.20); // Formaat van de klok
     this.initInteraction(); // Initialiseren van interactie
   }
 
@@ -49,34 +49,15 @@ export class Clock extends PIXI.Container{
       this.clockGraphics.addChild(numberText); 
     }
 
-
-    // Getallen op de wijzerplaat
-    const numberStyle = new PIXI.TextStyle({
-      fontSize: 50,
-      fill: 'black',
-    });
-
-    for (let hour = 1; hour <= 12; hour++) {
-      const angle = (hour / 12) * Math.PI * 2; // Hoek voor elk uur
-      const radius = 150;
-      const x = 200 + radius * Math.sin(angle);
-      const y = 200 - radius * Math.cos(angle);
-
-      const numberText = new PIXI.Text(hour.toString(), numberStyle);
-      numberText.anchor.set(0.5);
-      numberText.position.set(x, y);
-      this.clockGraphics.addChild(numberText); 
-    }
-
     // Lange wijzer (minuten wijzer)
     const longHandRotation = (this.halfHourOffset / 180) * Math.PI;
-    this.clockGraphics.lineStyle(9, 0xff0000);
+    this.clockGraphics.lineStyle(12, 0xff0000);
     this.clockGraphics.moveTo(200, 200);
     this.clockGraphics.lineTo(200 + 150 * Math.sin(longHandRotation), 200 - 150 * Math.cos(longHandRotation));
 
     // Kleine wijzer (urenwijzer)
     const smallHandRotation = (this.smallHandOffset / 180) * Math.PI;
-    this.clockGraphics.lineStyle(9, 0x0000ff); // Blauwe kleur
+    this.clockGraphics.lineStyle(12, 0x0000ff); // Blauwe kleur
     this.clockGraphics.moveTo(200, 200);
     this.clockGraphics.lineTo(200 + 80 * Math.sin(smallHandRotation), 200 - 80 * Math.cos(smallHandRotation));
 
